c10e31aa9ed3d66c9e364807dda22069eed5d914
LETME2X <76720988+LETME2X@users.noreply.github.com>
2024-05-21 19:40:54 +0000

    datatypes

---
+++ basics/datatype.js
@@ -0,0 +1,15 @@
+"use stricts";
+
+// alert(3+3)   // we are using nodejs not browser
+
+console.log(3+3)
+console.log("hi")
+
+// none
+// undefined
+// symbol - figma tool
+
+
+// obj
+console.log(typeof undefined); //undefined
+
